0abe1939c0916624f63de1b62d3ebda44ca8f2c6
Vikleo <vikleoboy@gmail.com>
2022-11-20 14:06:36 +0530

    first coomit

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Intro from "./Intro";
+import ServicesComp from "./Services__Comp";
+import "./style.css";
+class Services extends Component {
+  render() {
+    return (
+      <section id="services" className="container container__services">
+        <Intro />
+        <ServicesComp />
+      </section>
+    );
+  }
+}
+export default Services;
